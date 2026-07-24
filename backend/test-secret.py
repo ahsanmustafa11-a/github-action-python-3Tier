29f176ef7fb118d822ca3757e3d89668f13cfa53
Ahsan Mustafa <139356111+ahsanmustafa11-a@users.noreply.github.com>
2026-07-25 00:54:28 +0500

    Update comments and clarify testing instructions

--- backend/test-secret.py
+++ backend/test-secret.py
@@ -1,6 +1,6 @@
-# For testing uncomment the Github lines or add another if it not working.  When i try thi work for me.That's why comment the Github Line.
+# For testing Add anther Keys or orignal for testing.
 
 AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7ABCDEF"
 AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY1"
-# GITHUB_TOKEN = "ghp_Ejkec09cjd889fd89cc89c8sDAsa8saDSDsdada9Sfad"
+
 DB_PASSWORD = "SuperSecretPassword123!"
