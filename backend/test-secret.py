0c7e04aa132c14c9b4cb1f84885a6de735fdddfa
Ahsan Mustafa <139356111+ahsanmustafa11-a@users.noreply.github.com>
2026-07-25 00:52:48 +0500

    Comment out GITHUB_TOKEN in test-secret.py
    Comment out the GITHUB_TOKEN for testing purposes.

--- backend/test-secret.py
+++ backend/test-secret.py
@@ -1,4 +1,6 @@
+# For testing uncomment the Github lines or add another if it not working.  When i try thi work for me.That's why comment the Github Line.
+
 AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7ABCDEF"
 AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY1"
-GITHUB_TOKEN = "ghp_Ejkec09cjd889fd89cc89c8sDAsa8saDSDsdada9Sfad"
+# GITHUB_TOKEN = "ghp_Ejkec09cjd889fd89cc89c8sDAsa8saDSDsdada9Sfad"
 DB_PASSWORD = "SuperSecretPassword123!"
